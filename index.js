91de8912e7d0bb94d49cb0f8533ca5ed0fe19d9d
Blake <blakewrobertson@ymail.com>
2019-09-08 02:38:05 -0400

    Connect Database

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var express = require("express");
+var express = require('express');
 var app = express();
+
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://group3:group3cop4331@ec2-18-221-247-1.us-east-2.compute.amazonaws.com:27017/dummyDB';
+
+mongoose.connect(mongoDB, {useNewUrlParse: true});
+
+app.get('/', function (req, res) {
+  res.sendFile('home.html', { root: __dirname });
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
-app.get("/url", (req, res, next) => {
-res.json(["Tony"]);
-});
