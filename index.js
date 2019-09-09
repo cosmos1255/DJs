bd489bb98f260d3ba6b340ac8add0bd6ea5f3ec2
Blake <blakewrobertson@ymail.com>
2019-09-08 21:16:30 -0400

    Fixed Connection With Database

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://group3:group3cop4331@ec2-18-221-247-1.us-east-2.compute.amazonaws.com:27017/dummyDB';
+const mongo = require('mongodb').MongoClient
+const url = 'mongodb://group3:group3cop4331@ec2-18-221-247-1.us-east-2.compute.amazonaws.com:27017'
 
-mongoose.connect(mongoDB, {useNewUrlParse: true});
+mongo.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err, client) => {
+if (err) {
+  console.error(err)
+  return
+}
+})
 
 app.get('/', function (req, res) {
   res.sendFile('home.html', { root: __dirname });
-});
+})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
-});
+})
 
