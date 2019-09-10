174118e8117a6f53b64b7824ab8a3e00632302ee
Blake Robertson <42613658+blakewrobertson@users.noreply.github.com>
2019-09-10 02:37:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const path = require('path');
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://<group3>:<group3cop4331>@cluster0-2n09e.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
 
 
 app.use(express.static('public'));
