9cc132f5ef2f55e92a2efe1e0a01a47543b408cb
Jay Adams <46606151+jwadams26@users.noreply.github.com>
2019-09-17 03:32:07 -0400

    setting up for api communication
    Fixed a bug in the index.html and started the ground work for home.js to communicate with the api (filler atm, not working features)

--- home.js
+++ home.js
@@ -1,25 +1,32 @@
 function initLogin()
 {
-	alert("LOGIN TEST");
 	// collect all information
 	var lUser = document.getElementById('loginUsername').value;
 	var lPass = document.getElementById('loginPassword').value;
 
 	// send info to server
+	var api = "http://local:3000/api";
+	var apikey = "filler";
+	var url = api + "/" + lUser + "/" + lPass + "/" + apikey;
 
+	alert(url);
 	// recieve info and test
 }
 
 function initSignup()
-{	alert("SIGNUP TEST");
+{
 	// collect all information
 	var fiName = document.getElementById('fname').value;
 	var laName = document.getElementById('lname').value;
 	var email = document.getElementById('Email').value;
-	var lUser = document.getElementById('signupUsername').value;
-	var lPass = document.getElementById('signupPassword').value;
+	var sUser = document.getElementById('signupUsername').value;
+	var sPass = document.getElementById('signupPassword').value;
 
 	// send info to server
-	
+	var api = "http://local:3000/api";
+	var apikey = "filler";
+	var url = api + "/" + fiName + "/" + laName + "/" + email + "/" + sUser + "/" + sPass + "/" + apikey;
+
+	alert(url);
 	// recieve info and test
 }
