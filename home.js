d8c0603b5e4b47fa878dba459681f1407b0accd0
Jay Adams <46606151+jwadams26@users.noreply.github.com>
2019-09-17 01:25:36 -0400

    Fixed deleted html js
    The js for the html got deleted, so that was fixed. The js was also moved to its own file called home.js so that this issue can be avoided in the future.

---
+++ home.js
@@ -0,0 +1,16 @@
+document.getElementById("loginBtn").addEventListener("click", function(){
+	alert("LOGIN TEST");
+	// collect all information
+
+	// send info to server
+
+	// recieve info and test
+});
+	document.getElementById("signupBtn").addEventListener("click", function(){
+	alert("SIGNUP TEST");
+	// collect all information
+
+	// send info to server
+	
+	// recieve info and test
+	});
