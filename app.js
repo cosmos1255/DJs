2ea7cafdafcd38746038d8f5fbaccaf4ffd27595
cosmos1255 <45180546+cosmos1255@users.noreply.github.com>
2019-09-17 23:23:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index2.html');
+  res.sendFile(__dirname + '/index.html');
 });
 app.listen(3000, () => {
    console.log("Connection Successful");
