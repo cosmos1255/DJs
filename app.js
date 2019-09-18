b38f50a4d4a24d47b895973f556d222299b7a96b
cosmos1255 <45180546+cosmos1255@users.noreply.github.com>
2019-09-17 22:35:49 -0400

    new changes to contact page

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index2.html');
 });
 app.listen(3000, () => {
    console.log("Connection Successful");
